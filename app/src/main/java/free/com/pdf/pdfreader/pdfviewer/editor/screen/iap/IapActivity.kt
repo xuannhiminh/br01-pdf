f0cbb05ab4f44c98103bfd76e71321855f8de67b
Huy <huyhuy6620000@gmail.com>
2025-10-21 13:58:48 +0700

    Add unused import

--- app/src/main/java/free/com/pdf/pdfreader/pdfviewer/editor/screen/iap/IapActivity.kt
+++ app/src/main/java/free/com/pdf/pdfreader/pdfviewer/editor/screen/iap/IapActivity.kt
@@ -12,6 +12,7 @@ import android.graphics.drawable.LayerDrawable
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.widget.Toast
 import androidx.core.view.WindowInsetsControllerCompat
 import androidx.fragment.app.FragmentActivity
